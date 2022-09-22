583ba9c786b4353c91198e0c69ae08cae2e9b96e
alfisyahrinn <sahrin201@gmail.com>
2022-09-22 20:35:43 +0700

    menambahkan prak3

---
+++ Praktikum3Bab6/src/ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse {
+  public static void main(String[] args) {
+    int Grade = 1;
+    if (Grade > 90) {
+      System.out.println("Mantap Bor");
+    } else if (Grade > 60) {
+      System.out.println("Mantap");
+    } else {
+      System.out.println("Adeh gagal lu");
+    }
+  }
+}
---
+++ Praktikum3Bab6/src/switchjava.java
@@ -0,0 +1,19 @@
+public class switchjava {
+  public static void main(String[] args) {
+    int Grade = 90;
+    switch (Grade) {
+      case 100:
+        System.out.println("mantap excelent");
+        break;
+      case 90:
+        System.out.println("good job");
+        break;
+      case 80:
+        System.out.println("study again");
+        break;
+      default:
+        System.out.println("failed lu");
+        break;
+    }
+  }
+}
