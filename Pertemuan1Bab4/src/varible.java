b5835668a3837a10b22ace011477a1d260df5a14
alfisyahrinn <sahrin201@gmail.com>
2022-09-22 13:15:05 +0700

    update

---
+++ Pertemuan1Bab4/src/varible.java
@@ -0,0 +1,9 @@
+public class varible {
+  public static void main(String[] args) {
+    String Nama = "Alfi Syahrin";
+    int Umur = 19;
+
+    System.out.println(Nama);
+    System.out.println(Umur);
+  }
+}
